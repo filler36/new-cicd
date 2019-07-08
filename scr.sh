6c482ebf2c0cf4e0d1e7e888d6a154ebcdffadb2
filler36 <filler36@gmail.com>
2019-07-08 12:08:44 +0000

    mod

---
+++ scr.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+route -n | awk '/UG[ \t]/{print $2}' >> /tmp/hostip
+cat /tmp/hostip >> /etc/hosts
+cp /etc/hosts /tmp/hosts.new
+sed -i '${s/$/ dockerhost/}' /tmp/hosts.new
+cp -f /tmp/hosts.new /etc/hosts
+chmod 666 /var/run/docker.sock
